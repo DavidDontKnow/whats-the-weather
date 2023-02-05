7711c67af060b98ca2ca5375f43ff5bd2ae75c9a
DavidDontKnow <DavidDontKnow02@gmail.com>
2023-02-05 15:46:34 -0800

    getweather

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ $(function () {
         let result = btn.val();
         console.log(result);
         geoApi(result);
+        // fillData(result, temp, wind, humidity, icon)
         btn.val("");
     })
 
@@ -17,11 +18,51 @@ $(function () {
                 return response.json();
             })
             .then(function (data) {
-                console.log(data)
+                // console.log(data)
+                var lat = data[0].lat
+                var lon = data[0].lon
+                // console.log(lon, lat)
+                getWeather(lat, lon, result)
             });
     }
 
+    function getWeather(lat, lon, result) {
+        var requestURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=1fb8f45e0d479a31123acdde2c53eba3";
 
+        fetch(requestURL)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                var temp = data.main.temp
+                var wind = data.wind.speed
+                var humidity = data.main.humidity
+                var icon = data.weather[0].icon;
+                fillData(result, temp, wind, humidity, icon)
+            });
+
+    }
+
+    function fillData(result, temp, wind, humidity, icon) {
+        let cityBtn = $("#City-Buttons");
+        let newBtn = $("<button>");
+        newBtn.attr("type", "button");
+        newBtn.attr("class", "btn btn-secondary mx-3 my-1");
+        newBtn.attr("id", result);
+        newBtn.text(result.toUpperCase());
+        cityBtn.append(newBtn);
+        let city = $("#current-city")
+        city.text(result.toUpperCase())
+        img = $("#weather-icon")
+        img.attr("src", "http://openweathermap.org/img/wn/" + icon + "@2x.png")
+        tempDisplay = $("#current-temp")
+        tempDisplay.text(temp)
+        windDisplay = $("#current-wind")
+        windDisplay.text(wind)
+        humidityDisplay = $("#current-humidity")
+        humidityDisplay.text(humidity)
+
+    }
 
 
 
