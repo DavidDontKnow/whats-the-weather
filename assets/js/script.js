46ce2eda2eb506d33571756d67a0bbb3cc2a62d9
DavidDontKnow <DavidDontKnow02@gmail.com>
2023-02-04 22:59:51 -0800

    geoApi and search function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,35 @@
-console.log("JavaScript is ready to run");
+// elements\
+$(function () {
+
+    $(".btn-primary").on("click", function () {
+        let btn = $("input");
+        let result = btn.val();
+        console.log(result);
+        geoApi(result);
+        btn.val("");
+    })
+
+    function geoApi(result) {
+        var requestURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + result + "&appid=1fb8f45e0d479a31123acdde2c53eba3";
+
+        fetch(requestURL)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data)
+            });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+})
