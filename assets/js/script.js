81fafe6aeb37df76f06bb0f4a68fbd9372f30700
DavidDontKnow <daviddontknow02@gmail.com>
2023-02-08 08:24:45 -0800

    added btn-secondary response on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ $(function () {
         geoApi(result);
         // fillData(result, temp, wind, humidity, icon)
         btn.val("");
-        window.localStorage.setItem(result, JSON.stringify(cityObject))
+
     })
     cityObject = {}
 
@@ -19,7 +19,7 @@ $(function () {
                 return response.json();
             })
             .then(function (data) {
-                console.log(data)
+
                 var cityName = data[0].name
                 cityObject.cityName = cityName;
                 var lat = data[0].lat
@@ -63,7 +63,7 @@ $(function () {
             })
             .then(function (data) {
 
-                fillForecast(data)
+                fillForecast(data, result)
             });
 
     }
@@ -89,13 +89,14 @@ $(function () {
         humidityDisplay.text(humidity)
 
 
+
     }
 
-    function fillForecast(data) {
+    function fillForecast(data, result) {
 
 
         var list = [data.list[1], data.list[8], data.list[16], data.list[24], data.list[32]]
-        console.log(list)
+
         cityObject.forecast = list
         for (let i = 0; i < 5; i++) {
 
@@ -108,8 +109,6 @@ $(function () {
             iconDisplay = $("#icon-forecast-" + i)
             iconDisplay.attr("src", "http://openweathermap.org/img/wn/" + icon + "@2x.png")
 
-
-
             var temp = list[i].main.temp
             var forecastTemp = $("#temp-forecast-" + i)
             forecastTemp.text(temp)
@@ -121,24 +120,84 @@ $(function () {
             var humidity = list[i].main.humidity
             var forecastHumidity = $("#humidity-forecast-" + i)
             forecastHumidity.text(humidity)
+
         }
+        window.localStorage.setItem(result, JSON.stringify(cityObject))
     }
 
 
     $("body").on("click", function (e) {
-        if ($(event.target).hasClass("btn-secondary")) {
-            console.log("click")
-            $("")
-            loadButton()
-        } else
-            console.log("failed ")
+        var id = e.target.id
+
+        if ($(e.target).hasClass("btn-secondary")) {
+            loadButton(id)
+        }
     })
 
-    function loadButton(result) {
-        var btn = window.localStorage.getItem('perris');
+    function loadButton(id) {
+
+        var btn = window.localStorage.getItem(id);
         btnContent = JSON.parse(btn)
-        console.log(btnContent)
+
+
+        var list = btnContent
+        console.log(list)
+
+        function getCurrentWeather(btnContent) {
+            lat = btnContent.lat
+            lon = btnContent.lon
+
+            var requestURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=1fb8f45e0d479a31123acdde2c53eba3";
+
+            fetch(requestURL)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    var temp = data.main.temp
+                    var wind = data.wind.speed
+                    var humidity = data.main.humidity
+                    var icon = data.weather[0].icon;
+                    let city = $("#current-city");
+                    city.text(btnContent.cityName);
+                    img = $("#weather-icon");
+                    img.attr("src", "http://openweathermap.org/img/wn/" + icon + "@2x.png");
+                    tempDisplay = $("#current-temp");
+                    tempDisplay.text(temp);
+                    windDisplay = $("#current-wind");
+                    windDisplay.text(wind);
+                    humidityDisplay = $("#current-humidity");
+                    humidityDisplay.text(humidity);
+                });
+        }
+
+        getCurrentWeather(btnContent)
+
+        for (let i = 0; i < 5; i++) {
+
+            var date = list.forecast[i].dt_txt.split(" ")
+            dateDisplay = $("#forecast-" + i + "-date")
+            dateDisplay.text(date)
+
+            var icon = list.forecast[i].weather[0].icon
+            iconDisplay = $("#icon-forecast-" + i)
+            iconDisplay.attr("src", "http://openweathermap.org/img/wn/" + icon + "@2x.png")
+
+            var temp = list.forecast[i].main.temp
+            var forecastTemp = $("#temp-forecast-" + i)
+            forecastTemp.text(temp)
+
+            var wind = list.forecast[i].wind.speed
+            var forecastWind = $("#wind-forecast-" + i)
+            forecastWind.text(wind)
+
+            var humidity = list.forecast[i].main.humidity
+            var forecastHumidity = $("#humidity-forecast-" + i)
+            forecastHumidity.text(humidity)
+        }
     }
+
+
 
 
 })
